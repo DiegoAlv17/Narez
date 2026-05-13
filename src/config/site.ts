c81c7e2143ba5da1e98219e00f90836549c033af
DiegoAlv17 <diegonapa17@gmail.com>
2026-05-12 21:47:47 -0500

    ajusstes al cambio de letra

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Narez",
   description: "Soluciones de software personalizadas: Landing pages, CRM, ERPs e Inteligencia Artificial.",
-  url: "https://narez.com", // update later
+  url: "https://narez.pe", // update later
   social: {
     github: "https://github.com/diegoalv17",
     twitter: "https://twitter.com"
